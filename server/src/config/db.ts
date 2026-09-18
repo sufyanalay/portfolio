ca3fd779338512efe33820d61fc39b874afff10c
Sufyan Ali <sufyanali34602@gmail.com>
2026-09-18 11:55:31 +0500

    fffoof

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 export const connectDB = async () => {
+  if (isConnected) return;
+
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI as string);
+    isConnected = true;
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("MongoDB connection failed:", error);
-    process.exit(1);
+    throw error;
   }
 };
